5e0081d4bc34a1383c4e4a9bfaccb65c5a804b80
arachnelove <arachnelove@proton.me>
2022-07-17 16:59:39 -0700

    started file parsing logic

--- TODO.md
+++ TODO.md
@@ -5,8 +5,11 @@
 * should do markdown to html conversion, prob with discount
 * should have base boilerplate/template/whatever
 * should support general assets/static files - css, js, etc
+* need to think precisely about linking and page paths - should prob be learned from tags? or sep logic? plus what about links, are they via id??? input filename???
 
 * metadata support, like tags
 * plugins - via scripting? pipes? - to affect output
+* print stats after parsing - unconnected pages, tags discovered, etc
+* clientside fulltext search??? maybe w/ webasm???
 
 * tests lmao
--- src/main.c
+++ src/main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
-int main(){
-    printf("Hello world\n");
+int main(int argc, char *argv[]){
+    
+    DIR *siteDir;
+    struct dirent *member;
+    struct stat fileInfo;
+
+    //check arguments
+    if(argc != 2){
+        perror("Please provide (only) a site directory name\n");
+        return(1);
+    }
+
+    //open directory
+    siteDir = opendir(argv[1]);
+
+    if(siteDir==NULL){
+        perror("Error opening directory");
+        return(2);
+    }
+
+    while(member=readdir(siteDir)){
+        stat(member->d_name,&fileInfo);
+        if(S_ISDIR(fileInfo.st_mode)){
+            printf("path:%s", member->d_name);
+        }
+        printf(" dir?:%d\n", S_ISDIR(fileInfo.st_mode));
+    }
+
+    closedir(siteDir);
 }
